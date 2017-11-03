4f0807899d59b04e0b2ed02382ef253db99ded40
ridanyuan <ridan.yuan@ele.me>
2017-11-03 17:14:50 +0800

    线程的中断

---
+++ code/JavaConcurrency/src/main/java/chapter1/chapter1_4/PrimeGenerator.java
@@ -0,0 +1,35 @@
+package chapter1.chapter1_4;
+
+/**
+ * Created by yrd on 2017/11/3.
+ */
+public class PrimeGenerator extends Thread {
+
+    @Override
+    public void run() {
+        long number = 1L;
+        while (true) {
+            if (isPrime(number)) {
+                System.out.printf("Number %d is Prime", number);
+            }
+            if (isInterrupted()) {
+                System.out.printf("The Prime Generator has been Interrupted");
+                return;
+            }
+            number++;
+        }
+
+    }
+
+    private boolean isPrime(long number) {
+        if (number <= 2) {
+            return true;
+        }
+        for (long i = 2; i < number; i++) {
+            if ((number % i) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
